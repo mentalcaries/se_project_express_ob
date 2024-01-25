55095132c00157bf394acdef86be38bb82ce6233
Obbie Omoruyi <obbie.omoruyi@gmail.com>
2024-01-25 12:49:44 -0600

    --------

--- src/components/AddClothsButton.js
+++ src/components/AddClothsButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AddClothsButton extends Component{
     render(){
         return(
-            <button>click mee!!</button>
+            <button className='header__add-button header__text'>+ Add Cloths</button>
         )
     }
 }

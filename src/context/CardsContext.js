d74a749e0c1149d465cc9a7282d997901b1bf696
Abdurahman Omoruyi <abdurahmanomoruyi1@gmail.com>
2024-09-05 12:55:23 -0500

    Remove all files from repository

--- src/context/CardsContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const CurrentCardsContext = React.createContext();
-
-export default CurrentCardsContext;
--- src/context/CurrentTemperatureUnitContext.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const CurrentTemperatureUnitContext = React.createContext();
-
-export default CurrentTemperatureUnitContext ;
- 

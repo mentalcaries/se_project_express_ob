86d67514b6124a5f86e7497966f9a1fe528f6335
Abdurahman Omoruyi <abdurahmanomoruyi1@gmail.com>
2024-10-30 16:00:12 -0500

    --------

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ require('dotenv').config();
 
 const app = express();
 
-const { PORT = 3001 } = process.env
+const { PORT = 3003 } = process.env
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
